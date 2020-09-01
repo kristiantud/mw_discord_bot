4460320eda14d6f24bde076d45997e96a59c99e9
Kristian <krt916@usask.ca>
2020-09-01 01:46:49 -0600

    can now show recent matches on discord

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ async function getMatchInformation(matchId, platform) {
   const summary = await API.MWFullMatchInfowz(matchId, platform).catch((err) => console.log(err));
   //console.log(summary.weekly.mode.br_brquads);
   
+  let statDict = {'itachi' : 0,
+				  'Legacy0_o' : 0,
+				  'x3TuD3x' : 0,
+				  'FunGuy' : 0,
+				  'LazyFlame' : 0,
+				  'Simon_SaysHi' : 0,
+				  'BlankFrog' : 0,
+				  'CrispyCahl' : 0,
+				  'DanKo' : 0,
+				  'ElCapitan306' : 0,
+				  'Nickmercs' : 0,
+				  'SypherPk' : 0,
+				  'Trash_Fue' : 0,
+				  'Doozy' : 0,
+				  'K3' : 0};
   
   // go over player list and find known players, print their kills
   console.log("Match ID: " + matchId + "\n");
@@ -34,11 +49,18 @@ async function getMatchInformation(matchId, platform) {
 		  summary.allPlayers[i].player.username === "BlankFrog" ||
 		  summary.allPlayers[i].player.username === "CrispyCahl" ||
 		  summary.allPlayers[i].player.username === "DanKo" ||
-		  summary.allPlayers[i].player.username === "ElCapitan306" ){
-			client.channels.cache.find( channel => channel.name === 'test').send(summary.allPlayers[i].player.username + " got " + summary.allPlayers[i].playerStats.kills + " kills!" );
-			
+		  summary.allPlayers[i].player.username === "ElCapitan306" ||
+		  // some pros: 
+		  summary.allPlayers[i].player.username === "Nickmercs" ||
+		  summary.allPlayers[i].player.username === "SypherPk" ||
+		  summary.allPlayers[i].player.username === "Trash_Fue" ||
+		  summary.allPlayers[i].player.username === "Doozy" ||
+		  summary.allPlayers[i].player.username === "K3" ){
+			statDict[summary.allPlayers[i].player.username] = summary.allPlayers[i].playerStats.kills;
 		}
   }
+  
+  return statDict;
     
   
   /*
@@ -80,14 +102,41 @@ async function checkGames(gamertag, plataform){
 async function inspectForEvents(){
 	var matchid = getMatchId("her0#11341", "battle");
 	matchid.then(function(res){
-		
-		
+		let matchStats = getMatchInformation(res, "battle");
+		console.log("match info found. printing...");
+		matchStats.then(function(results){
+			console.log(results);
+		});
 	});
+}
+
+async function getRecentMatchStats(matchID, platform){
+	const login = await API.login("", "").catch((err) => console.log(err));
+	const summary = await API.MWFullMatchInfowz(matchID, platform).catch((err) => console.log(err));
+	let matchStats = {'kills' : 0,
+					  'deaths' : 0,
+					  'damage_done' : 0,
+					  'kdr' : 0,
+					  'team_placement' : 0};
+	// use matchID to find stats on itachi
+	for (x = 0; x < summary.allPlayers.length; x++){
+		if(summary.allPlayers[x].player.username === 'itachi'){
+			//console.log("itachi's kills: " + summary.allPlayers[x].playerStats.kills);
+			//console.log("itachi's placement: " + summary.allPlayers[x].playerStats.teamPlacement);
+			matchStats['kills'] = summary.allPlayers[x].playerStats.kills;
+			matchStats['deaths'] = summary.allPlayers[x].playerStats.deaths;
+			matchStats['damage_done'] = summary.allPlayers[x].playerStats.damageDone;
+			matchStats['kdr'] = summary.allPlayers[x].playerStats.kdRatio;
+			matchStats['team_placement'] = summary.allPlayers[x].playerStats.teamPlacement;
+		}
+	}
+	
+	return matchStats;
 }
 
 
 
-let gamesPlayed = checkGames("her0#11341","battle");
+
 
 /*
 gamesPlayed.then(function(res){
@@ -125,6 +174,8 @@ matchId.then(function(res){
 });
 */
 
+
+let gamesPlayed = checkGames("x3TuD3x","xbl");
 //client.channels.cache.find(channel => channel.name === 'test').send("you just finished a match. will inspect...");
 client.login(token);
 
@@ -145,10 +196,44 @@ client.on('message',function(message){
 	// tell the bot to start looking for new matches
 	if (message.content === '!start'){
 		var interval = setInterval(function(){
-			client.channels.cache.find(x => x.name === 'test').send("this is just a test of interval. don't panic!");
-			
-			
-		},5000);
+			// client.channels.cache.find(x => x.name === 'test').send("this is just a test of interval. don't panic!");
+			// now look for games
+			gamesPlayed.then(function(res){
+				let newGameChecker = checkGames("x3TuD3x", "xbl");
+				newGameChecker.then(function(result){
+					if (result > res){
+						// new game found
+						console.log("new game found. fetching information...");
+						gamesPlayed = gamesPlayed + 1;
+						inspectForEvents();
+					}else{
+						console.log("no new games found.");
+						client.channels.cache.find(x => x.name === 'test').send("No new games yet.");
+					}
+				});
+			});	
+		},60000);
+	}
+	
+	// ask bot for information on last game
+	if (message.content === '!recentMatch'){
+		let matchId = getMatchId("her0#11341","battle");
+		matchId.then(function(res){
+			let recentStats = getRecentMatchStats(res,"battle");
+			recentStats.then(function(results){
+				//console.log(results);
+				const embed = new Discord.MessageEmbed()
+					.setTitle('itachi\'s Recent Match Stats')
+					.setDescription('Kills: ' + results['kills'] + 
+									'\nDeaths: ' + results['deaths'] +
+									'\nDamage Dealt: ' + results['damage_done'] + 
+									'\nKill/Death Ratio: ' + results['kdr'] + 
+									'\nPlacement: ' + results['team_placement'])
+					.setColor('#f50057');
+					message.channel.send(embed);
+			});
+		});
+		
 	}
 	
 	// disconnect the bot completely
